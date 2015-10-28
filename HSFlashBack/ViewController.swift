b2e9ecba7ed0ea7bf11e9f7f5961b1ad4c068e73
Matthew King <mwk5wy@labmini15.local>
2015-10-28 02:53:35 -0400

    First commit! Implemented: Home Screen, Settings Page, Salt Page
    To do:
    Make Salt Page load different Hero Portraits

--- HSFlashBack/ViewController.swift
+++ HSFlashBack/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class Home_Page: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,3 +23,76 @@ class ViewController: UIViewController {
 
 }
 
+class Settings_Page: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var picker: UIPickerView!
+    var pickerData: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // Connect data:
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        // Input data into the Array:
+        pickerData = ["Druid", "Hunter", "Mage", "Paladin", "Priest",  "Rogue", "Shaman", "Warlock", "Warrior"]
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        
+            let svc = segue.destinationViewController as! Salt_Page
+            svc.heroSelected = (picker.selectedRowInComponent(<#T##component: Int##Int#>))
+        
+    }
+
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+}
+
+
+
+
+class Salt_Page: UIViewController {
+    
+    var heroSelected:Int!
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet var saltLabel: UIView!
+    var imageName: [String] = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
