da96dd9b260f780175828bed96344855ec6b10ba
Matthew King <mwk5wy@labmini11.local>
2015-11-12 20:56:00 -0500

    adding sound emotes

--- HSFlashBack/Salt_Page_Controller.swift
+++ HSFlashBack/Salt_Page_Controller.swift
@@ -8,13 +8,16 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class Salt_Page: UIViewController {
     
     
+    @IBOutlet weak var emoteButton: UIButton!
     @IBOutlet weak var saltLabel: UILabel!
     @IBOutlet weak var heroPortrait: UIImageView!
     var heroString: String!
+    var audioPlayer: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +27,32 @@ class Salt_Page: UIViewController {
         self.heroPortrait.image = UIImage(named: heroString)
         let heroName: String = heroString.stringByReplacingOccurrencesOfString("_", withString: " ")
         saltLabel.text = heroName + " makes you salty"
-        
-        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func emotePress(sender: UIButton) {
+        
+        playSound("audiofile")
+        
+    }
+    
+    //play a sound on tap
+    func playSound(soundName: String)
+    {
+        let sound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(soundName, ofType: "mp3")!)
+        do{
+            self.audioPlayer = try AVAudioPlayer(contentsOfURL:sound)
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        }catch {
+            print("Error getting the audio file")
+        }
+    }
+    
     
 }
