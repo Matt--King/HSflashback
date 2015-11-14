1e2e12267440a7b9db8eb11373b69415d14e6fe6
Matt--King <kingmatt333@hotmail.com>
2015-11-14 18:07:29 -0500

    small changes

--- HSFlashBack/GameScene.swift
+++ HSFlashBack/GameScene.swift
@@ -13,9 +13,7 @@ class GameScene: SKScene {
     var audioPlayer: AVAudioPlayer!
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        
-        
-    }
+           }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         /* Called when a touch begins */
