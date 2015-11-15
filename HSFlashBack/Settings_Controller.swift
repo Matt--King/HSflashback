144a199f2068aa996c1c4206d1d0d7de4d205909
Aakash Shah <ahs2hd@labmini10.local>
2015-11-14 19:25:09 -0500

    text editing corrected. difficulties with alert for saving

--- HSFlashBack/Settings_Controller.swift
+++ HSFlashBack/Settings_Controller.swift
@@ -17,40 +17,10 @@ class Settings_Page: UIViewController, UIImagePickerControllerDelegate, UINaviga
     let imagePicker = UIImagePickerController()
     @IBOutlet weak var heroPortrait: UIView!
     @IBOutlet weak var nameField: UITextField!
+    var pictureTaken = false
     
     
-    // Custom Hero Portrait Feature
-    
-    @IBAction func loadImage(sender: UIButton) {
-        if UIImagePickerController.isSourceTypeAvailable(.Camera){
-        
-            imagePicker.allowsEditing = false
-            imagePicker.sourceType = .Camera
-            presentViewController(imagePicker, animated: true, completion: nil)
-        } else {
-            var alertController:UIAlertController?
-            alertController = UIAlertController(title: "Oops",
-                message: "We can't seem to find your device's camera.", preferredStyle: .Alert)
-            
-            let firstAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
-            alertController!.addAction(firstAction)
-            self.presentViewController(alertController!, animated: true, completion: nil)
-
-        }
-        
-    }
-    
-    
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-            //  imageView.contentMode = .ScaleAspectFill
-            self.imageView.image = pickedImage
-        }
-        
-        dismissViewControllerAnimated(true, completion: nil)
-    }
     // Share App Feature
-    
     @IBAction func showShare(sender: UIButton) {
         let firstActivityItem = "I'm loving the Hearthstone Flashbacks App!"
         
@@ -60,13 +30,61 @@ class Settings_Page: UIViewController, UIImagePickerControllerDelegate, UINaviga
         
     }
     
+    // Custom Hero Portrait Feature
+   
+    @IBAction func loadImage(sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.Camera){
+        
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .Camera
+            presentViewController(imagePicker, animated: true, completion: nil)
+        } else {
+            var alertController:UIAlertController?
+            alertController = UIAlertController(title: "Oops",
+                message: "We can't find your device's camera.", preferredStyle: .Alert)
+            
+            let firstAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+            alertController!.addAction(firstAction)
+            self.presentViewController(alertController!, animated: true, completion: nil)
+
+        }
+        
+    }
     
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            //  imageView.contentMode = .ScaleAspectFill
+            self.imageView.image = pickedImage
+            pictureTaken = true
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     
-    // onDownloadButton
+    // Limit text field length
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        if (range.length + range.location > textField.text!.characters.count ){
+            return false;
+        }
+        
+        let length = textField.text!.characters.count + string.characters.count - range.length
+        
+        return length <= 15
+        
+    }
+    
+    // Closes text field keyboard on return press
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    // Saving the Custom Hero Portrait
     @IBAction func saveHeroPortrait(sender: AnyObject) {
         // access big view
         // turn view into uiima
@@ -74,12 +92,9 @@ class Settings_Page: UIViewController, UIImagePickerControllerDelegate, UINaviga
         heroPortrait.layer.renderInContext(UIGraphicsGetCurrentContext()!)
         let screenShot = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
-        
         UIImageWriteToSavedPhotosAlbum(screenShot, self, "image:didFinishSavingWithError:contextInfo:", nil)
         
     }
-    
-    
     
     func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafePointer<Void>) {
         if error == nil {
@@ -92,13 +107,31 @@ class Settings_Page: UIViewController, UIImagePickerControllerDelegate, UINaviga
             presentViewController(ac, animated: true, completion: nil)
         }
     }
-    
-    
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        nameField.resignFirstResponder()
-        return true
+
+    // Back button check for edits
+    @IBAction func backCheck(sender: UIButton){
+        // pictureTaken is a non optional bool. isEmpty is an optional Bool. ?? means if Bool is not nil, value, else, false
+        if ((pictureTaken) || (nameField.text?.isEmpty != true))  {
+            var alertController:UIAlertController?
+            alertController = UIAlertController(title: "Careful",message: "Picture and text edits will not be saved!", preferredStyle: .Alert)
+            
+            let firstAction = UIAlertAction(title: "Keep Editing", style: UIAlertActionStyle.Cancel, handler: nil)
+            alertController!.addAction(firstAction)
+            
+            //let secondAction = UIAlertAction(title: "Leave", style: UIAlertActionStyle.Default,
+            //    handler: alertSegueHandler )
+            //alertController!.addAction(secondAction)
+            self.presentViewController(alertController!, animated: true, completion: nil)
+        
+        }
     }
     
+    func alertSegueHandler(alert: UIAlertAction!) {
+        performSegueWithIdentifier("SetToHomeSeg", sender: nil)
+    }
+    
+    
+    // Defaults / Misc
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
